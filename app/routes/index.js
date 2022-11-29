3e200be15b817e07348f78ddff63f24388a3ea60
Tia Eastwood <teastwood88@gmail.com>
2022-11-29 12:05:13 +0000

    make extra api call in index route to get all data

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,11 +2,19 @@ import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
   async model() {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon');
-    const pokes = await response.json();
+    let detailedPokemon = [];
 
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon');
+    const pokes = await res.json();
     const data = pokes.results;
 
-    return data;
+    for (const pokemon of data) {
+      const res = await fetch(`${pokemon.url}`);
+      const pokeData = await res.json();
+      detailedPokemon.push(pokeData);
+    }
+
+    console.log(detailedPokemon);
+    return detailedPokemon;
   }
 }
